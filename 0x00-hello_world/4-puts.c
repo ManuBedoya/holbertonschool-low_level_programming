0a5e6298f5f0842b456add86f98ca585df21042c
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-03 13:11:40 -0500

    4 Script is done!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+1 #include <stdio.h>
+  2
+  3 /**
+  4  * main - Entry point
+  5  *
+  6  * Return: Always 0 (Success)
+  7  */
+8 int main(void)
+9 {
+ 10
+	 11         puts("\"Programming is like building a multilingual puzzle");
+ 12         return (0);
+ 13 }
