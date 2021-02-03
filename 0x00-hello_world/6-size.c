9d0d248dd525062872c40a0a747493cc36d5b9f3
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-03 14:34:12 -0500

    fix 6V0

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a int: 4 byte(s)\n");
 	printf("Size of a long int: 4 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
-	printf("Size of a float: 4 byte(s)\n");
+	printf("Size of a float: 4 byte(s)");
 	return (0);
 }
