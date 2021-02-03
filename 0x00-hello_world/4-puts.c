e4533debd16c2e8dbcd8851d00742a55eaba705a
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-03 13:20:42 -0500

    fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-1 #include <stdio.h>
-  2
-  3 /**
-  4  * main - Entry point
-  5  *
-  6  * Return: Always 0 (Success)
-  7  */
-8 int main(void)
-9 {
- 10
-	 11         puts("\"Programming is like building a multilingual puzzle");
- 12         return (0);
- 13 }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
