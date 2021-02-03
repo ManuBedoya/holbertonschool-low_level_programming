8b48a90853c69af2a775f798eef94c3445d8a643
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-03 13:55:55 -0500

    6 is done!

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of a int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+	return (0);
+}
