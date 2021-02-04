411d305f2459fbc008c760dbd9bc7b59b0e085f2
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-03 22:55:59 -0500

    fixed 9 script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, ", 50);
-	write(2, "2015-10-19\n", 12);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
 	return (1);
 }
