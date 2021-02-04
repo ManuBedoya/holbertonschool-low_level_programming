31454c1154fb0a221481cf65245e4265c7e57644
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-04 18:10:38 -0500

    5 Done!

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main -> entry
+ *
+ *Return: 0 always
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
