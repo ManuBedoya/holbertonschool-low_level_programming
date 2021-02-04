e80f3119fc9a69a127d58b24b0d9b40144801fcd
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-04 12:16:41 -0500

    Alphabet is done !

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main -> Print the alphabet
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int a = 97;
+
+	for (a; a < 123 ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
