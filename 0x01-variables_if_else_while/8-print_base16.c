56f8523ebdc03cf4e603bd0e22e46d58c61afc70
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-04 19:28:10 -0500

    6 Done!

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main -> entry
+ *
+ *Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	char b[] = "0123456789abcdf";
+
+	for (a = 0; a < 16; a++)
+	{
+		putchar(b[a]);
+	}
+	putchar('\n');
+	return (0);
+}
