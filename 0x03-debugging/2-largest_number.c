cd6a507ff1734a6cc60d56fe27f31fb483438471
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-09 13:00:25 -0500

    Fixed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,10 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && a > c)
-	{
 		largest = a;
-	}
 	else if (b > a && b > c)
-	{
 		largest = b;
-	}
-	else if (c > a && c > b)
-	{
+	else
 		largest = c;
-	}
-
 	return (largest);
 }
