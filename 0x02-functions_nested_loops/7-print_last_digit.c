d1e41d9eb1a3c5df6e2ea10f14402dc55f21193b
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-09 13:17:53 -0500

    update the 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include <stdlib>
 #include "holberton.h"
 
 /**
@@ -8,8 +9,7 @@
  */
 int print_last_digit(int r)
 {
-	if(r < 0)
-		r = r * -1;
+	r = abs(r);
 	r = r % 10;
 	_putchar(r + '0');
 	return (r);
