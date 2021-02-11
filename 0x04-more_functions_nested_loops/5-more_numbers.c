ae591550545af9eec6ed0e698440c4db376058e0
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-11 14:35:16 -0500

    update5V0

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,15 +14,12 @@ void more_numbers(void)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			if (a <= 9)
-				_putchar(a + '0');
-			else
-			{
+			if (a > 9)
 				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-			}
+			_putchar((a % 10) + '0');
 		}
 	_putchar('\n');
 	tenTimes++;
 	}
 }
+
