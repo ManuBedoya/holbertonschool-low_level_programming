491bea4b9ac728ffbbbf22583a06036919336b0d
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-15 17:56:17 -0500

    PrintNElements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array
+ * @a: array
+ * @n: n elements
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
