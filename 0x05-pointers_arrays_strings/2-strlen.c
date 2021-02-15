d787348bf86ac03eab6710ae2840bab1c27b0fa8
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-15 12:35:54 -0500

    LenghtOfString Done!

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - calcuate the lenght of a string
+ * @s: Array of strings
+ * Return: a length of *s
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
