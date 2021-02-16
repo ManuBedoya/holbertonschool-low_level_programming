1c414682d864d121ed8c764060bea51d4f5828e2
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-16 10:42:14 -0500

    Update7V0

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,13 +2,28 @@
 #include <string.h>
 
 /**
+ *calculateLenght - Calculate number of strings
+ * @s: String
+ * Return: the length of the number!!
+*/
+
+int calculateLenght(char *s)
+{
+	int count = 0;
+
+	while (s[count] != 0)
+		count++;
+	return (count);
+}
+
+/**
  * puts_half - Print the half string
  * @str: String
 */
 void puts_half(char *str)
 {
 	int i;
-	int count = strlen(str);
+	int count = calculateLenght(str);
 	int start = count / 2;
 
 	if (count % 2 != 0)
