b43293c43573a4e2de79807a0c9e9234750c8a9d
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-16 10:39:29 -0500

    Update3V0

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,29 @@
 #include <stdio.h>
 
 /**
+ *calculateLenght - Calculate number of strings
+ * @s: String
+ * Return: the length of the number!!
+*/
+
+int calculateLenght(char *s)
+{
+	int count = 0;
+
+	while (s[count] != 0)
+		count++;
+	return (count);
+}
+
+/**
  * _puts - Print a string
  * @str: String
 */
 void _puts(char *str)
 {
-	puts(str);
+	int i;
+
+	for (i = 0; i < calculateLenght(str); i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
