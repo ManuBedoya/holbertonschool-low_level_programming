52b63b494d0008929c4e7c02622da5a8a6ab002d
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-16 09:39:27 -0500

    Update6V0

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,7 @@ void puts2(char *str)
 	int i;
 
 	_putchar(str[0]);
-	for (i = 0; i < rand() % 11; i++)
-		_putchar(rand() % (11 + 100));
+	for (i = 0; i < 5; i++)
+		_putchar(i + 5);
 	_putchar('\n');
 }
