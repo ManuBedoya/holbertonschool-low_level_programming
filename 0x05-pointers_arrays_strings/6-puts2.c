a54f503b43c65b952035a347671786ea53b92394
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-16 10:34:59 -0500

    Update6V0

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
+ *calculateLenght - Calculate number of strings
+ * @s: String
+ * Return: the length of the number!!
+*/
+
+int calculateLenght(char *s)
+{
+	int count = 0;
+
+	while (s[count] != 0)
+		count++;
+	return (count);
+}
+
+/**
  * puts2 - print any character wit the first letter
  * @str: String
 */
@@ -9,7 +24,7 @@ void puts2(char *str)
 	int i;
 
 	_putchar(str[0]);
-	for (i = 1; i < 5; i++)
-		_putchar(i * 2);
+	for (i = 1; i < calculateLenght(str); i += 2)
+		_putchar(str[i + 1]);
 	_putchar('\n');
 }
