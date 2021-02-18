a0f17068e7e680aa73e7d294a016ffa2f94ca6ba
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-18 11:05:21 -0500

    ReverseArray Done!

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - Reverse the array
+ *
+ * @a: array
+ * @n: number of elements
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j = n - 1;
+	char aux;
+
+	for (i = 0; j != i; i++, j--)
+	{
+		aux = a[i];
+		a[i] = a[j];
+		a[j] = aux;
+	}
+}
