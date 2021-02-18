67102022e8c99016b4a863576ff1f40e5d057947
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-18 16:05:37 -0500

    Update8V0

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,24 +8,49 @@
  */
 char *rot13(char *c)
 {
-	int i;
+	int i = 0;
+	int j = 0;
 
-	for (i = 0; c[i] != '\0'; i++)
+	while (c[i] != '\0')
 	{
-		if (c[i] >= 'a' && c[i] <= 'z')
+	back:
+		if ((c[i] < 'a' || c[i] > 'z') && (c[i] < 'A' || c[i] > 'Z'))
+		{
+			i++;
+			goto back;
+		}
+
+		else
+		{
+			for (; c[i] >= 'a' && c[i] <= 'z' && j != 1; j--)
+			{
+				int positionLetter = c[i] - 97;
+				int enocodeLetter = (positionLetter + 13) % 26;
+
+				c[i] = enocodeLetter + 97;
+				i++;
+				goto back;
+			}
+			int positionLetter = c[i] - 65;
+			int enocodeLetter = (positionLetter + 13) % 26;
+
+			c[i] = enocodeLetter + 65;
+			i++;
+		}
+
+		/*if (c[i] >= 'a' && c[i] <= 'z')
 		{
 			int positionLetter = c[i] - 97;
 			int enocodeLetter = (positionLetter + 13) % 26;
 
 			c[i] = enocodeLetter + 97;
+			continue;
 		}
-		else if (c[i] >= 'A' && c[i] <= 'Z')
-		{
-			int positionLetter = c[i] - 65;
-			int enocodeLetter = (positionLetter + 13) % 26;
 
-			c[i] = enocodeLetter + 65;
-		}
+		int positionLetter = c[i] - 65;
+		int enocodeLetter = (positionLetter + 13) % 26;
+
+		c[i] = enocodeLetter + 65;*/
 	}
 	return (c);
 }
