b9fbf5aa7518341e63fba2ba2c392ae236af0bf9
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-18 15:09:23 -0500

    Update7V0

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,20 +10,30 @@
 char *leet(char *c)
 {
 	int i;
+	char encode[109];
+
+	encode[97] = '4';
+	encode[65] = '4';
+	encode[101] = '3';
+	encode[69] = '3';
+	encode[111] = '0';
+	encode[79] = '0';
+	encode[116] = '7';
+	encode[84] = '7';
+	encode[108] = '1';
+	encode[76] = '1';
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if (c[i] == 'a' || c[i] == 'A')
-			c[i] = '4';
-		else if (c[i] == 'e' || c[i] == 'E')
-			c[i] = '3';
-		else if (c[i] == 'o' || c[i] == 'O')
-			c[i] = '0';
-		else if (c[i] == 't' || c[i] == 'T')
-			c[i] = '7';
-		else if (c[i] == 'l' || c[i] == 'L')
-			c[i] = '1';
-	}
+		if (c[i] == 'a' || c[i] == 'A' || c[i] == 'e' ||
+		    c[i] == 'E' || c[i] == 'o' || c[i] == 'O' ||
+		    c[i] == 't' || c[i] == 'T' || c[i] == 'l' ||
+		    c[i] == 'L')
+		{
+			int cast = c[i];
 
+			c[i] = encode[cast];
+		}
+	}
 	return (c);
 }
