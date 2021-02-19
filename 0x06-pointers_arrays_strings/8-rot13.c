237fc22c35d733df558c6c2d085b04e50033de2f
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-19 08:11:35 -0500

    Update8V2

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -10,30 +10,29 @@ char *rot13(char *c)
 {
 	int i = 0;
 	int j = 0;
+	int positionLetter;
 
+back:
 	while (c[i] != '\0')
 	{
-back:
 	if (((c[i] < 'a' || c[i] > 'z') && (c[i] < 'A' || c[i] > 'Z')))
 	{
 		i++;
 		goto back;
-		}
-		else
+	}
+	else
+	{
+		for (; c[i] >= 'a' && c[i] <= 'z' && j != 1; j--)
 		{
-			for (; c[i] >= 'a' && c[i] <= 'z' && j != 1; j--)
-			{
-				int positionLetter = c[i] - 97;
-
-				c[i] = ((positionLetter + 13) % 26) + 97;
-				i++;
-				goto back;
-			}
-			int positionLetter = c[i] - 65;
-
-			c[i] = ((positionLetter + 13) % 26) + 65;
+			positionLetter = c[i] - 97;
+			c[i] = ((positionLetter + 13) % 26) + 97;
 			i++;
+			goto back;
 		}
+		positionLetter = c[i] - 65;
+		c[i] = ((positionLetter + 13) % 26) + 65;
+		i++;
+	}
 	}
 	return (c);
 }
