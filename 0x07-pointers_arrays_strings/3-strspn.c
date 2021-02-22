4db331ea9012b32a105312f4bb71b6339ef37a93
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-22 17:42:08 -0500

    Update3V0

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,12 +12,17 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int ocurrences = 0;
 	int i = 0, j = 0;
 
-	for (i = 0; s[i] != ' '; i++)
+	for (i = 0; s[i]; i++)
 	{
+		if (accept[j] == '\0')
+			return (ocurrences);
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				ocurrences++;
+				break;
+			}
 		}
 	}
 	return (ocurrences);
