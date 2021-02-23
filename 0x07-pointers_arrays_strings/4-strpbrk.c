bb190e38586ae5b0a642aec80501a58bf07fc885
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-22 20:10:03 -0500

    Update4V0

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	int i = 0, j = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
+
