a86f73dac385107fc1d3f8937ef173b541eb8feb
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-24 16:23:06 -0500

    6IsDone!

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,32 @@
 #include "holberton.h"
 
-/***/
+/**
+ *operation - recursive
+ *@nConstant: Number
+ *@nVariety: Number
+ *@original: Number
+ *Return: 0->false 1->True
+ **/
+int operation(int nConstant, int nVariety, int original)
+{
+	if (nConstant == 1)
+		return (1);
+	else if (nConstant * nVariety == original)
+		return (0);
+	if (nConstant == nVariety)
+		return (operation(nConstant - 1, 2, original));
+	else
+		return (operation(nConstant, nVariety + 1, original));
+
+}
+/**
+ *is_prime_number - Call the function recursive
+ *@n: Number
+ *Return: 0->False 1->True
+ */
 int is_prime_number(int n)
 {
-	if (n == 0)
-		return (1);
-	else if (n == 1)
+	if (n <= 0 || n == 1 || n > 1000)
 		return (0);
-
-	if (is_prime_number(n - 2) *
-	    is_prime_number(n - 1) == 7)
-		return (0);
-	else
-		return is_prime_number(n - 1);
+	return (operation(n, 2, n));
 }
