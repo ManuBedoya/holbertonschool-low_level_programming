152d6aee558ccfa79d9d2f957cd0ea6794c5ff51
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-24 14:36:27 -0500

    FixThe7v0!

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
-#include <string.h>
+
+/**
+ *_strlen_recursion - length of String
+ *@s: String
+ *Return: lenght
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+	else
+		return (0);
+}
+
 
 /**
  *is_palindromeAux - Veriify if a string is palindrome
@@ -10,7 +26,8 @@
  */
 int is_palindromeAux(char *s, int i, int e)
 {
-	if ((i == e) || (strlen(s) % 2 == 0 && i + 1 == e))
+	if ((i == e) || (_strlen_recursion(s) % 2 == 0 &&
+			 i + 1 == e))
 		return (1);
 	if (s[i] == s[e])
 		return (is_palindromeAux(s, i + 1, e - 1));
@@ -26,10 +43,12 @@ int is_palindrome(char *s)
 {
 	if (s[0] == '\0')
 		return (1);
-	else if (s[0] == s[strlen(s) - 1])
+	else if (s[0] == s[_strlen_recursion(s) - 1])
 	{
-		return (is_palindromeAux(s, 0, strlen(s) - 1));
+		return (is_palindromeAux(s, 0,
+					 _strlen_recursion(s) - 1));
 	}
 	else
 		return (0);
 }
+
