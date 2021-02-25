e43fecefd6625499f170cb061196a2b53ef0ee26
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-02-24 19:44:53 -0500

    Update5V1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,32 @@
 #include "holberton.h"
 
+/**
+ *operation - operation of square of a number
+ *@original: Number that never change
+ *@nVariety: Dinamic Number
+ *Return: Square of n
+ */
 int operation(int original, int nVariety)
 {
-	if(nVariety == 0)
+	if (nVariety == 0)
 		return (-1);
 	if (nVariety * nVariety == original)
 		return (nVariety);
 	else
-		return operation(original, nVariety - 1);
+		return (operation(original, nVariety - 1));
 }
 
 
-/***/
+/**
+ *_sqrt_recursion - Print square of a number
+ *@n: Number
+ *Return: Square of n
+ */
 int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
 	else if (n == 1)
 		return (1);
-	return operation(n, n / 2);
+	return (operation(n, n / 2));
 }
