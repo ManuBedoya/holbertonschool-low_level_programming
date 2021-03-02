3044d307a8d78c90589d45e49f251bacfef63766
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-02 15:33:32 -0500

    Update0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,16 +8,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *p;
-unsigned int i;
-if (size == 0)
-return ('\0');
-p = malloc(sizeof(char) * size);
-if (p == 0)
-return (NULL);
-for (i = 0; i < size; i++)
-{
-p[i] = c;
-}
-return (p);
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return ('\0');
+	p = malloc(sizeof(char) * size);
+	if (p == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
 }
