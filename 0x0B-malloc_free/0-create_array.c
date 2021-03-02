8ef306ae018d56ad21225b6dfd64326731c357d0
Manuel Bedoya <47682759+ManuBedoya@users.noreply.github.com>
2021-03-02 15:29:48 -0500

    Update0V6

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,28 +1,23 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
- **create_array - Create a dinamic array
- *@size: Size of Array
- *@c: Inicializate all with c character
- *Return: Array
+ * create_array - create an array
+ *@c: the characters
+ *@size: the size of characters
+ *Return: reyturn 0
  */
-
 char *create_array(unsigned int size, char c)
 {
-	if (size <= 0)
-		return ('\0');
-
-	char *s;
-	unsigned int i = 0;
-
-	s = (char *) malloc(size * sizeof(char));
-
-	if (s == NULL)
-		return ('\0');
-
-	for (i = 0; i < size; i++)
-		s[i] = c;
-	return (s);
-	free(s);
+char *p;
+unsigned int i;
+if (size == 0)
+return ('\0');
+p = malloc(sizeof(char) * size);
+if (p == 0)
+return (NULL);
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+return (p);
 }
