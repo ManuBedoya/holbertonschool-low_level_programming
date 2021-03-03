08405a256a91e3ee38334116866bbdb1c2e5e6c0
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-02 22:04:42 -0500

    Update1V3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,8 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+	{}
 
 	str2 = malloc((i + 1) * sizeof(char));
 
