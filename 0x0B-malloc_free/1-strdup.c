56478092da1debadbb4b512f70e9045260a8fb1f
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-02 22:01:50 -0500

    Update1V2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,23 +9,21 @@
 
 char *_strdup(char *str)
 {
-	int i = 0, count = 0;
+	int i = 0;
 	char *str2;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
-		count++;
+	for (i = 0; str[i] != '\0'; i++);
 
-	str2 = malloc((count + 1) * sizeof(char));
+	str2 = malloc((i + 1) * sizeof(char));
 
 	if (str2 == NULL)
 		return (NULL);
 
-	*str2 = *str;
 
-	for (i = 1; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		str2[i] = str[i];
 	}
