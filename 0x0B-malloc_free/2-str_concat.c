0d2bb8c8f66a020fe49dc70f78ec0c10d397581b
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-02 23:43:34 -0500

    Update2V5

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,12 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *s3;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 
