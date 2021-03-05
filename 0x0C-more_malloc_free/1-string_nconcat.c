904be25b32f724b095dffa1eb0623805f18b4f9c
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-05 11:42:51 -0500

    Update1V1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,16 +16,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
-
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+
+	}
 	while (s1[len1] || s2[len2])
 	{
 		if (s1[len1])
 			len1++;
 		if (s2[len2])
 			len2++;
+
 	}
 	len1++;
 	if (n >= len2)
@@ -35,13 +40,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s3 == NULL)
 		return (NULL);
-
 	for (k = 0; s1[k]; k++)
 		s3[k] = s1[k];
-
-	for (; k - len1 + 1 < n && s2[k - len1 + 1]; k++)
+	for (; (k - len1) + 1 < n && s2[k - len1 + 1]; k++)
+	{
 		s3[k] = s2[k - len1 + 1];
 
+	}
 	s3[k] = '\0';
+
 	return (s3);
 }
