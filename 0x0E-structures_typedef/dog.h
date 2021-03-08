9f93dcdfaa898b3e4aa8802fa1486219caf9ac68
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-08 14:05:29 -0500

    Add styles

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,19 @@
 #ifndef DOG
 #define DOG
+
+/**
+ *struct dog - Values of the dog
+ *@name: Name
+ *@age: Age
+ *@owner: Owner
+ *
+ *Description: Structure of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-#endif // DOG
+#endif
