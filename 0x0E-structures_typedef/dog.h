bf604ffe980311b2e0b1ca49d39d942776ef4ed1
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-03-08 14:49:02 -0500

    Add new type to dog strucuture

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,12 @@
  *Description: Structure of the dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
