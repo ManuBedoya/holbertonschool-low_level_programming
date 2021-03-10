44ac27938dc1a47dee8f043759551f258dd80ff7
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-10 13:44:33 -0500

    Add condition when action and array are null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (action != NULL && array != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
