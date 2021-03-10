e0edf9c5d5626308c742e39a5d02ed29d2f6f3e6
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-10 13:48:18 -0500

    Add condition if is null in 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]))
-			return (i);
+
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+		return (-1);
 	}
 	return (-1);
 }
