eca06582f0c699335c853d5a1c2d8eb0c10c1340
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-10 13:51:21 -0500

    Add condition if is null

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,21 +8,25 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (i < 5)
+	if (s != NULL)
 	{
-		if (ops[i].op[0] == *s)
-			return (ops[i].f);
-		i++;
+		op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}
+		};
+		int i = 0;
+
+		while (i < 5)
+		{
+			if (ops[i].op[0] == *s)
+				return (ops[i].f);
+			i++;
+		}
+		return (NULL);
 	}
 	return (NULL);
 }
