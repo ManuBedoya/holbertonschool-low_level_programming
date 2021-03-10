7b1efbb89b1cb2c5bb8e560bf1f10f00ba5e6bc2
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-10 13:54:18 -0500

    Quit the condition and travels until 5

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,25 +8,21 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-	if (s != NULL)
-	{
-		op_t ops[] = {
-			{"+", op_add},
-			{"-", op_sub},
-			{"*", op_mul},
-			{"/", op_div},
-			{"%", op_mod},
-			{NULL, NULL}
-		};
-		int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-		while (i < 5)
-		{
-			if (ops[i].op[0] == *s)
-				return (ops[i].f);
-			i++;
-		}
-		return (NULL);
+	while (i < 6)
+	{
+		if (ops[i].op[0] == *s)
+			return (ops[i].f);
+		i++;
 	}
 	return (NULL);
 }
