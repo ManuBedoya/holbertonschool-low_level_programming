18d13236f716620f372d7da061bdcb52d31bbf30
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-11 17:07:42 -0500

    Fix the position condition separator different to null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,10 +17,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(ap, n);
 		for (i = 0; i < n; i++)
 		{
-			if (i + 1 != n)
-				printf("%i%s", va_arg(ap, int), separator);
-			else
-				printf("%i", va_arg(ap, int));
+			printf("%i", va_arg(ap, int));
+			if (i + 1 != n && separator != NULL)
+				printf("%s", separator);
 		}
 		printf("\n");
 		va_end(ap);
