fba98417e49b0f891bc43697b600c26b54f7195e
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-19 11:39:07 -0500

    remove the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,16 +8,3 @@ void __attribute__ ((constructor)) firstFunction()
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense ");
-	printf("of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
