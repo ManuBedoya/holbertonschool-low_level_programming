98bd900ea9d4126c7538069c13d4feb50173a2f8
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-23 09:46:32 -0500

    [9-insert] add condition in for

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,10 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || *head == NULL || newNode == NULL)
 		return (NULL);
-	for (i = 0; i < (idx - 1); i++)
+	for (i = 0; i < (idx - 1) && tmp->next; i++)
 	{
-		if (tmp == NULL)
-			return (NULL);
 		tmp = tmp->next;
 	}
 	newNode->n = n;
