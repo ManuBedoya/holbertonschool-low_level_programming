642c6a7fba5b824c45afa599a4b091083dad347f
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-25 16:29:15 -0500

    Update header and add the finction decimal-binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,60 @@
 #include "holberton.h"
-
 /**
- * print_binary2 - calculate from decimal to binary
- * @n: Number in decimal
+ * _pow - return pow with base 2
+ * @expo: Exponent
+ * Return: the pow of the number 2 whit exponent expo
 */
-void print_binary2(unsigned long int n)
+unsigned long int _pow(int expo)
 {
-	if (n != 0)
+	int i = 0, result = 1;
+
+	if (expo == 0)
+		return (1);
+	for (i = 0; i < expo; i++)
 	{
-		print_binary2(n / 2);
-		_putchar(n % 2 + '0');
+		result = result * 2;
 	}
+	return (result);
 }
 /**
- * print_binary - calculate from decimal to binary
- * @n: Number in decimal
+ * print_binary - calculate from decumal to binary
+ * @n: Decimal
+ * Return: Binary
 */
 void print_binary(unsigned long int n)
 {
+	unsigned long int tmp = 0, sum = 0;
+	int i;
+
 	if (n == 0)
 		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
 	else
-		print_binary2(n);
+	{
+		while (_pow(tmp) < n)
+			tmp++;
+		if (_pow(tmp) == n)
+		{
+			_putchar('1');
+			for (i = (tmp - 1); i >= 0 ; i--)
+			{
+				_putchar('0');
+			}
+		}
+		else
+		{
+			for (i = (tmp - 1); i >= 0; i--)
+			{
+				if (sum + _pow(i) <= n)
+				{
+					_putchar('1');
+					sum += _pow(i);
+				}
+				else
+					_putchar('0');
+			}
+		}
+	}
 }
 
