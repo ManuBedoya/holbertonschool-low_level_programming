3db216aa51536aad4ae121d6863427632e19a788
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-29 14:24:58 -0500

    [2-append_text_to_file.c]Remove the trunc

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	len = getLen(text_content);
-	fd = open(filename, O_APPEND | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
 	nWrite = write(fd, text_content, len);
