408d756c5b8e489515991dcf52deef12b8c67573
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-03-29 13:49:46 -0500

    [0-read_textfile.c] change some things

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,15 +10,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t n;
 	char *buf[letters + 1];
-	int fd = open(filename, O_RDONLY);
+	int fd;
 
-	if (filename == NULL)
+	if (filename == '\0')
 		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	n = read(fd, buf, letters);
+	if (n == -1)
+		return (0);
 	buf[n] = '\0';
 	close(fd);
 	n = write(STDOUT_FILENO, buf, (n - 1));
+	if (n == -1)
+		return (0);
 	return (n);
 }
