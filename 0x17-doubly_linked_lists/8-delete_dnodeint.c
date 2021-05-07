f22045d417339ab8902ac5a7482422bf12f9557f
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-05-06 20:31:25 -0500

    [DeleteNodeInt] -1 when nothing

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if ((*head)->next == NULL)
 		{
-			return (1);
+			return (-1);
 		}
 		*head = (*head)->next;
 		(*head)->prev = NULL;
