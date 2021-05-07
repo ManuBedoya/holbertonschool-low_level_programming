494ffb0ac5b364ca95f9b2bd9785aee86cd2f688
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-05-06 19:44:26 -0500

    [AddNodeInt] avoid when malloc fails

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,6 +9,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node = malloc(sizeof(dlistint_t));
 
+	if (node == NULL)
+		return (NULL);
 	if (head == NULL)
 		return (NULL);
 	node->n = (int) n;
