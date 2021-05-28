876c1051f57fb9715865abbf11b0a890ea5f7783
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-28 15:42:35 -0500

    [set] update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	key_copy = (char *) key;
 	new_node = malloc(sizeof(hash_node_t));
 
-	if (strcmp(key_copy, "") || new_node == NULL)
+	if (key_copy == NULL || new_node == NULL)
 		return (0);
 
 	new_node->key = key_copy;
