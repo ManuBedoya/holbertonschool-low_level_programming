24eaa2f5b84089680181cee5eb1a6736e2d354a9
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-28 15:33:43 -0500

    [create] add condition when size is less

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
-	if (size <= NULL)
+	if (size <= 0)
 		return (NULL);
 
 	new_hash_table = malloc(sizeof(hash_table_t));
