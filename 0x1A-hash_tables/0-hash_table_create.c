cd902bb5c43227eb5aaeb2ce3888791cc4aa0067
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-28 15:37:01 -0500

    [create] update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,21 +11,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
 	hash_node_t **array;
-	unsigned long int i;
 
 	if (size <= 0)
 		return (NULL);
 
 	new_hash_table = malloc(sizeof(hash_table_t));
-	array = malloc(sizeof(hash_node_t) * size);
-	if (array == NULL || new_hash_table == NULL)
+	if (new_hash_table == NULL)
 		return (NULL);
-
-	for (i = 0; i < size; i++)
+	array = calloc(size, sizeof(hash_node_t));
+	if (array == NULL)
 	{
-		array[i] = NULL;
+		free(new_hash_table);
+		return (NULL);
 	}
-
 
 	new_hash_table->size = size;
 	new_hash_table->array = array;
